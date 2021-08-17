019e75fdc1dfb9275c712a5fdc1410a9726276a0
BA Dijkman <ba.dijkman@icloud.com>
2021-08-17 15:36:35 +0200

    delete double apiUrl

--- api-clients.js
+++ api-clients.js
@@ -1,8 +1,8 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-const apiUrl2 = "https://pokeapi.co/api/v2/pokemon/";
+
 
 const getPokemon = async (ID) => {
-  const ApiEndPoint = `${apiUrl2}${ID}`;
+  const ApiEndPoint = `${apiUrl}${ID}`;
   try {
     const response = await fetch(ApiEndPoint, { methode: "GET" });
     return await response.json();
